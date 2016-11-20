61444b6c56c2c39c20e9ee5a5db7d4c393f403f4
LostAnt <djerland69@gmail.com>
2016-11-21 02:23:52 +0700

    Добавлены проекты. Добавлены сущности.

--- DomainShop/Person.cs
+++ DomainShop/Person.cs
@@ -6,7 +6,8 @@ namespace DomainShop
     public class Person
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateTime RegistrationDate { get; set; } 
     }
 }

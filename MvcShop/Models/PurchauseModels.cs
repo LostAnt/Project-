51077437c5d3a4b3bff1c6bd14313ec7c3b24153
RINCH <RINCH@DESKTOP-KQL19TJ>
2016-12-14 19:01:25 +0700

    Создание админ панели, небольшие обновление репозиториев, контекстов, интерфейсов.

---
+++ MvcShop/Models/PurchauseModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace MvcShop.Models
+{
+    public class PurchauseModels 
+    {
+        public long Id { get; }
+        public string Buyer { get; set; }
+        public string Seller { get; set; }
+        public string Property { get; set; }
+        public DateTime Date { get; }
+    }
+}
